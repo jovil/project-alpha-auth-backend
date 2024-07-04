0061c7d834d382f32f1cc9a21820393e91846d77
Jovil <jovilh@gmail.com>
2024-07-05 02:16:17 +0800

    checkpoint

--- app.js
+++ app.js
@@ -14,26 +14,12 @@ const dbConnect = require("./db/dbConnect");
 // execute database connection
 dbConnect();
 
-// Curb Cores Error by adding a header here
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
-
 app.use(
   cors({
     origin: "*",
   })
 );
-console.log("1");
+
 // body parser configuration
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
