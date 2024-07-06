d3528146ed5ff8c109b6c9649788c617a0ff2205
Jovil <jovilh@gmail.com>
2024-07-06 18:11:22 +0800

    fix: mongoose error convert to string

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,13 +1,11 @@
 // external imports
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
 async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
-    .connect(
-      process.env.DB_URL,
-    )
+    .connect(`'${process.env.DB_URL}'`)
     .then(() => {
       console.log("Successfully connected to MongoDB Atlas!");
     })
