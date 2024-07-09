9ec3997c2995c1179fdee2c610c1ff2b45d84c65
Jovil <jovilh@gmail.com>
2024-07-09 17:45:42 +0800

    save profile and login user on registration

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,44 +3,68 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../model/userModel");
+const Profile = require("../model/profileModel");
 
 // register endpoint
-router.post("/register", (request, response) => {
-  // hash the password
-  bcrypt
-    .hash(request.body.password, 10)
-    .then((hashedPassword) => {
-      // create a new user instance and collect the data
-      const user = new User({
-        email: request.body.email,
-        password: hashedPassword,
-      });
+router.post("/register", async (request, response) => {
+  try {
+    const { email, password, userName } = request.body;
 
-      // save the new user
-      user
-        .save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          response.status(201).send({
-            message: "User Created Successfully",
-            result,
-          });
-        })
-        // catch error if the new user wasn't added successfully to the database
-        .catch((error) => {
-          response.status(500).send({
-            message: "Error creating user",
-            error,
-          });
-        });
-    })
-    // catch error if the password hash isn't successful
-    .catch((e) => {
-      response.status(500).send({
-        message: "Password was not hashed successfully",
-        e,
-      });
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create a new user instance and collect the data
+    const newUser = await new User({
+      email: email,
+      password: hashedPassword,
+      userName: request.body.userName,
     });
+
+    // save the new user
+    await newUser.save();
+
+    // Create a profile for the new user
+    const newProfile = new Profile({ user: newUser._id, email });
+    await newProfile.save();
+
+    console.log("New user created:", newUser);
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return response.status(404).send({ message: "Email not found" });
+    }
+
+    // compare the password entered and the hashed password found
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (!passwordCheck) {
+      return response.status(400).send({ message: "Passwords do not match" });
+    }
+
+    // create JWT token
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        userEmail: user.email,
+      },
+      "RANDOM-TOKEN",
+      { expiresIn: "24h" }
+    );
+
+    //   return success response
+    response.status(200).send({
+      _id: user._id,
+      message: "Registration and Login Successful",
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    response.status(500).send({
+      message: "Error during registration and login",
+      error,
+    });
+  }
 });
 
 // login endpoint
