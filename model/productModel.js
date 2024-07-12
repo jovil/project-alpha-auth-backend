b4f0a5e038e5f821264c0477503ef10924ca9c3b
Jovil <jovilh@gmail.com>
2024-07-12 16:04:50 +0800

    troubleshooting

--- model/productModel.js
+++ model/productModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  fileUrl: [String],
+  fileUrl: [{ String }],
   productName: String,
   productDescription: String,
   productPrice: String,
